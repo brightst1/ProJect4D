716a3c6dd49b61b6a42b079b2ad5e30d129b4a21
DESKTOP-6M378DP\Sup Bro! <supawit_st1@hotmail.com>
2018-01-23 21:48:14 +0700

    update models

--- projectsenior/models/request.js
+++ projectsenior/models/request.js
@@ -1,6 +1,11 @@
 var mongoose = require("mongoose")
+var ObjectId = mongoose.Types.ObjectId;
 
 var request = mongoose.Schema({
+    Username = String,
+    Providername = String,
+    Service_id = {type : Schema.Types.ObjectId , ref : 'service'},
+    location = { lat : Number, long:Number }
 
 })
 
--- projectsenior/models/service.js
+++ projectsenior/models/service.js
@@ -1,8 +1,10 @@
 var mongoose = require("mongoose")
 
+
 var service = mongoose.Schema({
     detail  = String,
-    
+    price   = String,
+    statusFlag = { type : Number , default : 0} //0 wait , 1 going , 2 done
 })
 
 module.exports = mongoose.model("service",service)
